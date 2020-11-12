48778a52411376fc7059c6780a4c856a1b83c8ca
reza <raftx24@gmail.com>
2020-11-12 13:04:14 +0330

    first commit

---
+++ src/bulma/register.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import App from '@enso-ui/ui/src/core/app';
+import BookmarksState from './components/settings/BookmarksState.vue';
+
+Vue.component('setting-bookmarks-state', BookmarksState);
+App.registerSettingsItem('setting-bookmarks-state', 10);
