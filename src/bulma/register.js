158504a9c4e6ac3441952f8b39357d4b39eb1d89
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-06 16:02:22 +0000

    Replace enso-ui to liberu-ui

--- src/bulma/register.js
+++ src/bulma/register.js
@@ -1,4 +1,4 @@
-import App from '@enso-ui/ui/src/core/app';
+import App from '@liberu-ui/ui/src/core/app';
 import BookmarksState from './components/settings/BookmarksState.vue';
 
 App.registerSettingsItem('bookmarks-state', BookmarksState, 500);
