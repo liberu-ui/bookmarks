ccce9cdd217960ddbe7f34ff817c1f598b4107a4
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-07 20:40:33 +0000

    Replace JavaScript with TypeScript

--- src/bulma/register.js
+++
@@ -1,4 +0,0 @@
-import App from '@liberu-ui/ui/src/core/app';
-import BookmarksState from './components/settings/BookmarksState.vue';
-
-App.registerSettingsItem('bookmarks-state', BookmarksState, 500);
